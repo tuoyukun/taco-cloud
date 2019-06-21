19bd9f58bbbacf6490f925f996acaedd043f3b3c
yukun.tyk <yukun.tyk@alibaba-inc.com>
2019-06-21 21:48:50 +0800

    init commit

--- src/test/java/com/tuoyukun/tacocloud/HomeControllerTest.java
+++ src/test/java/com/tuoyukun/tacocloud/HomeControllerTest.java
@@ -1,4 +1,30 @@
 package com.tuoyukun.tacocloud;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.hamcrest.Matchers.containsString;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void testHomePage()throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect((ResultMatcher) content().string(containsString("Welcome to...")));
+    }
 }
